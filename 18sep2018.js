82fab020dc245ef83d964e8d68eeca0226544605
Yalamarthi Rajesh <rajeshchowdary3230@gmail.com>
2018-09-19 19:48:02 +0530

    Add files via upload

---
+++ 18sep2018.js
@@ -0,0 +1,70 @@
+﻿$(document).ready(Iamready);
+
+
+function Iamready() {
+
+        $("#hai").click(function () {
+            $("p").hide();
+
+        });
+    
+        $("#hello").click(function () {
+            $("p").show();
+        });
+    
+}
+
+ //using * selector
+var elementcount = $("#test").find("*").length;
+$("body").prepend("<h2>" + elementcount + "Elements found </h3>");
+
+ //fading
+
+$(document).ready(fadein);
+$(document).ready(fadeout);
+
+function fadein() {
+    $(".b1").click(function () {
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(2000);
+    });
+
+}
+
+function fadeout() {
+    $(".b2").click(function () {
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(2000);
+    });
+
+}
+// sum of all
+
+$(document).ready(sum);
+
+
+function sum() {
+
+    $("#myTable").on('input', '.txtCal', function () {
+        var calculatedTotalsum = 0;
+        $("#myTable .txtCal").each(function () {
+            var gettextboxvalue = $(this).val();
+            if ($.isNumeric(gettextboxvalue)) {
+                calculatedTotalsum = calculatedTotalsum + parseFloat(gettextboxvalue);
+            }
+        });
+        $("#total_sum_value").html(calculatedTotalsum);
+    });
+}
+
+
+
+
+
+     
+
+
+
+
---
+++ 19sep2018.js
@@ -0,0 +1,101 @@
+﻿
+//siblings
+
+$(document).ready(function () {
+    siblings1();
+    rajesh();
+    catt();
+    filters();
+    newoperation();
+    Childern();
+
+    function siblings1() {
+
+        //console.log($(".check .rajesh").siblings.css({ "color": "red", "border": "2px solid red" }));
+        $(".check .rajesh").siblings().css({ "color": "red", "border": "2px solid red" });
+    }
+
+    function rajesh() {
+      
+        $("#att").click(alert1)
+        $("#BtnID").click(alert2)
+    
+
+        function alert1() {
+            alert("Dont touch Me")
+        }
+        function alert2() {
+            empty();
+            remove();
+            detach();
+            unwrap();
+            alert("Go Away")
+
+
+            function empty() {
+
+                var a = $(".dol").empty();
+                console.log(a);
+
+            }
+
+            function remove() {
+                var b = $(".dol2").remove();
+                console.log(b);
+            }
+
+            function detach() {
+              var c=  $(".dol3").detach();
+                console.log(c);
+            }
+
+
+            function unwrap() {
+                var c = $(".dol4").unwrap();
+                console.log(c);
+            }
+          
+           
+        }   }
+
+
+    function catt() {
+        $("#ca").attr("Id", "cattribute")
+
+        $("#Toggle").click(checktoggle)
+
+        //toggle
+        function checktoggle() {
+            $("#Toggle1Check").toggle("slow", "swing")
+        }
+    }
+    // filters plus chaining
+    function filters() {
+        $("#Filters").click(checkfilter)
+        function checkfilter() {
+            $("div").first().css({ "background-color": "yellow" }).slideUp(2000).slideDown(2000);
+        }
+    }
+    //using this and on
+    function newoperation() {
+       
+     
+        $("#no4").on("click", function () {
+            var a = $("#no1").val();
+            var b = $("#no2").val();
+            var c = a + b;
+            $("#no3").val(c); 
+        })
+       
+    }
+
+    function Childern() {
+        $("#child").children().css({ "color": "red", "border": "2px solid red" })
+    }
+
+
+
+});
+
+
+
